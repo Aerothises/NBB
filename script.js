d40f9287476c61f951a756dff750247109b5931f
Aerothises <63618221+Aerothises@users.noreply.github.com>
2020-05-21 18:49:33 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("sideNav").style.width = "12%";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
-  }
