58818d03199b5f6ea1cd0a3c71687e4cb0544138
Aerothises <dboulay95@gmail.com>
2020-07-27 19:12:35 -0700

    Updated main site details.
    Updated site 0.0.02

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-function openNav() {
-    document.getElementById("sideNav").style.width = "12%";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("sideNav").style.width = "0";
-  }
+var page = document.getElementById("_document")
+var pageCtn = document.getElementById("pageCtn")
+var listenPage = document.getElementById("_listenPage")
+var followPage = document.getElementById("_followPage")
+var aboutPage = document.getElementById("_aboutPage")
+var btns = document.getElementsByClassName("contentPage")
 
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.link-nav-2')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+var navBarMain = document.getElementById("_navBar-main")
+var sticky = navBarMain.offsetTop
+
+window.onscroll = function myFunction1() {
+    if (window.pageYOffset >= sticky) {
+        navBarMain.classList.add("sticky")
+    } else {
+        navBarMain.classList.remove("sticky")
     }
-  }
+}
+
+
+function showContent(e) {
+/*Iterate through contentPages*/
+    for (let index = 0; index < btns.length; index++) {
+        const btn = btns[index];
+/*Hides them*/
+        btn.classList.add("hiddenPage")
+    }
+/*Shows the page that was clicked*/
+
+e.classList.remove("hiddenPage")
+                        }
+;
+
